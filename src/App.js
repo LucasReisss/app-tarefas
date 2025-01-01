8ed05531c7c11af2816da6ae0f45479d093b18b0
Lucas <54081741+LucasReisss@users.noreply.github.com>
2025-01-01 09:51:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css'
+import {useState} from "react";
+import Cabecalho from "./components/Cabecalho";
+import NovaTarefa from "./components/NovaTarefa";
+import ListaTarefas from "./components/ListaTarefas";
+
+export default function App() {
+    const [tarefas, setTarefas] = useState([
+        {id: 1, titulo: 'Estudar React', completa: false},
+        {id: 2, titulo: 'Estudar Node', completa: true}
+    ])
+
+    return (
+        <div className='App'>
+            <Cabecalho/>
+            <NovaTarefa tarefas={tarefas} setTarefas={setTarefas} />
+            <ListaTarefas titulo="Tarefas Pendentes"
+                          tarefasFiltradas={tarefas.filter(t => !t.completa)}
+                          tarefas={tarefas} setTarefas={setTarefas}
+            />
+            <ListaTarefas titulo="Tarefas Concluídas:"
+                          tarefasFiltradas={tarefas.filter(t => t.completa)}
+                          tarefas={tarefas} setTarefas={setTarefas}
+            />
+        </div>
+    )
+}
